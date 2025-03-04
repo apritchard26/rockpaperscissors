1a810c6ba833d8732100ea6aa7e4af3afdbe9f09
apritchard26 <158027847+apritchard26@users.noreply.github.com>
2025-03-04 14:18:18 -0800

    Added getHumanChoice, playround functions

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
+
+humanScore = 0;
+computerScore = 0;
+
+
 function getComputerChoice(){
     num = Math.random();
     if(num < 0.333)
@@ -7,7 +12,42 @@ function getComputerChoice(){
     else
         return "scissors";
 }
-for(i=0;i<20;i++){
-    console.log(getComputerChoice());
+
+function getHumanChoice(){
+    humanChoice = prompt("Pick either rock, paper, or scissors.");
+    humanChoice = humanChoice.toLowerCase();
+    return humanChoice;
 }
 
+function playRound(humanChoice,computerChoice){
+    //computer wins
+    if(computerChoice=="rock" && humanChoice=="scissors"){
+        console.log("Computer wins!");
+        computerScore++;
+    }
+    else if(computerChoice=="paper" && humanChoice=="rock"){
+        console.log("Computer wins!");
+        computerScore++;
+    }
+    else if(computerChoice=="scissors" && humanChoice=="paper"){
+        console.log("Computer wins!");
+        computerScore++;
+    }
+    //human wins
+    else if(computerChoice=="rock" && humanChoice=="paper"){
+        console.log("Human wins!");
+        humanScore++;
+    }
+    else if(computerChoice=="scissors" && humanChoice=="rock"){
+        console.log("Human wins!");
+        humanScore++;
+    }
+    else if(computerChoice=="paper" && humanChoice=="scissors"){
+        console.log("Human wins!");
+        humanScore++;
+    }
+    //tie
+    else if(computerChoice==humanChoice)
+        console.log("Tie");
+
+}
